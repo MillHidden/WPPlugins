048b54b74853473ab04400de0fef4dd8d9e6fbf7
MillHidden <jeremy.hindenoch@gmail.com>
2016-07-06 11:07:41 +0200

    Update PGMTwitch.php

--- PGMTwitch/PGMTwitch.php
+++ PGMTwitch/PGMTwitch.php
@@ -9,13 +9,18 @@
 
   class PGMTwitch_plugin
   {
-  	public function __construct()
-  	{
+  	protected $client_id;
+  	protected $client_secret;
   		
+
+  	public function __construct($client_id = 't6a5c7t3yr8usx1yh3kuse4w3uwlq5r', $client_secret = 'o6phe6l29f9v5798tqvs15nk7f2b3im')
+  	{
+  		$this->client_id = $client_id;
+  		$this->client_secret = $client_secret;
   		
   		add_action('widgets_init', function()
 			{register_widget('PGM_live_twitch_widget');}
-		);
+		);		
   	}
 
   	public function embed_live_twitch($atts)
@@ -55,52 +60,45 @@
 			</iframe>';
 
 		return $output;
-  	}
+  	}  	
 
-  	/*public function embed_datas_twitch($atts)
-  	{
-  		extract(shortcode_atts(array(
-  			'username' => "puregamemedia"
-  		  ), $atts));
+  	public function get_url_contents($url) {
+		$crl = curl_init();
+		$timeout = 5;
 
-  		$videourl = "https://api.twitch.tv/kraken/streams/".$username;
-  		$videos = self::file_get_contents_curl($videourl);
-  		$obj = json_decode($videos, true);
-  
-		$infos['title'] = $obj['stream']['channel']['status'];
-		$infos['url'] = $obj['stream']['channel']['url'];		
-		$infos['game'] = $obj['stream']['game'];
-		$infos['viewers'] = $obj['stream']['viewers'];
-		$infos['views'] = $obj['stream']['channel']['views'];
-		$infos['followers'] = $obj['stream']['channel']['followers'];
-
-		$output = '<div class="PGM-datas-twitch">
-						<ul>
-							<li>'.$infos['title'].'</li>
-							<li>'.$infos['url'].'</li>							
-							<li>'.$infos['game'].'</li>
-							<li>'.$infos['viewers'].'</li>
-							<li>'.$infos['views'].'</li>
-							<li>'.$infos['followers'].'</li>		
-						</ul>
-				</div>';
+		curl_setopt($crl, CURLOPT_URL, $url);
+		curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, $timeout);		
 		
-  		return $output;
-  	}*/
+		$ret = curl_exec($crl);
+		curl_close($crl);
 
-  	public function file_get_contents_curl($url) {
-		$ch = curl_init();
+		return $ret;
+	}
 
-		curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+	public function post_url_contents($url, $fields)
+	{
+		foreach($fields as $key => $value)
+		{
+			$fileds_string = $key.'='.urlencode($value).'&';
+		}
+		rtrim($fields_string, '&');
 
-		$data = curl_exec($ch);
-		curl_close($ch);
+		$crl = curl_init();
+		$timeout = 5;
 
-		return $data;
+		curl_setopt($crl, CURLOPT_URL,$url);
+	    curl_setopt($crl, CURLOPT_POST, count($fields));
+	    curl_setopt($crl, CURLOPT_POSTFIELDS, $fields_string);
+
+	    curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
+
+	    $ret = curl_exec($crl);
+	    curl_close($crl);
+
+	    return $ret;
+
 	}
 
 	public function embed_datas_twitch($atts)
@@ -117,7 +115,7 @@
    		  ), $atts));
 
   		$videourl = "https://api.twitch.tv/kraken/streams/".$username;
-  		$videos = self::file_get_contents_curl($videourl);
+  		$videos = self::get_url_contents($videourl);
   		$obj = json_decode($videos, true);
   		
   		if ($datas === "all")
@@ -151,12 +149,53 @@
 
 		return $infos;
   	}
+
+  	public function get_user()
+  	{
+				
+  		if ($_GET['logout'] === "true")
+		{
+			unset($_SESSION["access_token"]);
+			
+		}
+
+  		if (!isset($_SESSION['return_url']))
+		{
+			$_SESSION['return_url'] = $_SERVER["REQUEST_URI"];  				
+		}
+  		
+  		include_once 'twitchtv.php';
+		$twitchtv = new TwitchTv($this->client_id, $this->client_secret); 		
+		if (!isset($_SESSION["access_token"])) {
+		    echo '<a href="' . $twitchtv->authenticate() . '">M\'identifier</a><br/>';
+		    $ttv_code = $_GET['code'];
+		    $access_token = $twitchtv->get_access_token($ttv_code);
+		    $_SESSION["access_token"] = $access_token;
+		} else {
+		    $access_token = $_SESSION["access_token"];
+		}
+		$user_name = $twitchtv->authenticated_user($access_token);
+
+		if (isset($user_name)) {		    
+		    echo 'Thank you ' . $user_name . '!  Authentication Completed!';
+		    echo '<a href="'.$_SESSION['return_url'].'?=logout">Logout</a><br/>';
+
+		    $is_following = $twitchtv->is_following_channel("puregamemedia", $access_token);
+
+		    if ($is_following)
+  			{
+  				echo('<a href="#">Ne plus suivre</a>');
+  			}
+  			else
+  			{
+  				echo('<a href="#">Suivre la chaîne</a>');
+  			}
+		}
+	}	
+ 	
   }
 
 add_shortcode('embedTwitch', array('PGMTwitch_plugin', 'embed_live_twitch'));
 add_shortcode('embedChat', array('PGMTwitch_plugin', 'embed_chat_twitch'));
 add_shortcode('embedDatas', array('PGMTwitch_plugin', 'embed_datas_twitch'));
-
-
-
-  
+add_shortcode('embedUser', array('PGMTwitch_plugin', 'get_user'));
